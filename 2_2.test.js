334fb485fe13424e510bdde2916f97e65b969622
vupn <vupham140199@gmail.com>
2023-09-18 12:23:59 +0700

    complete 3.2

--- 2_2.test.js
+++
@@ -1,9 +0,0 @@
-// Test
-const selection_sort_second = require("./2_2");
-
-test("returns the longest word in the list", () => {
-  expect(selection_sort_second(["apple", "banana", "pear"])).toBe("banana");
-  expect(selection_sort_second(["cat", "dog", "elephant"])).toBe("elephant");
-  expect(selection_sort_second(["hello", "world"])).toBe("hello");
-  expect(selection_sort_second(["a", "bb", "ccc"])).toBe("ccc");
-});
