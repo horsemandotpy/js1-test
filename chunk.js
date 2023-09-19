ad8d090ff86ca94bd6778726fc557b844a3c67ab
vupn <vupham140199@gmail.com>
2023-09-19 17:50:18 +0700

    Change Solution Draft

--- chunk.js
+++ chunk.js
@@ -2,25 +2,8 @@
 // Take in an array and an split number
 // Return array that contain subarr each have split number length
 // Solution
-// Make array holder
-// For each sub array
-// Make sub array holder
-// For each element in array
-// Push it into sub array holder
-// Push subarray into array holder
-// Return array holder
 const chunk = (array, number) => {
-  const arr = [];
-  let sub = [];
-  for (let i = 0; i < array.length; i++) {
-    if (sub.length < number) {
-      console.log(array[i])
-      sub.push(array[i]);
-    } else {
-      arr.push(sub);
-      sub = [];
-    }
-  }
+  
   return arr;
 };
 
